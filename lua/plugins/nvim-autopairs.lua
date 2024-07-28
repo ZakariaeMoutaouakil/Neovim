6fac5dfa8435d0e1bbc1dca30c44b89f07fe8a71
Zakariae Moutaouakil <moutaouakilzakariae.cpgea@gmail.com>
2024-07-28 19:42:38 +0200

    Modified nvim-autopairs.lua

--- lua/plugins/nvim-autopairs.lua
+++ lua/plugins/nvim-autopairs.lua
@@ -5,9 +5,9 @@ return {
     opts = {
       check_ts = true,
       ts_config = {
-        lua = {'string'},  -- don't add pairs in lua string treesitter nodes
-        javascript = {'template_string'},  -- don't add pairs in javascript template_string treesitter nodes
-        java = false,  -- don't check treesitter on java
+        lua = {'string'},
+        javascript = {'template_string'},
+        java = false,
       },
       disable_filetype = { "TelescopePrompt" },
       fast_wrap = {
@@ -23,7 +23,58 @@ return {
     },
     config = function(_, opts)
       local npairs = require("nvim-autopairs")
+      local Rule = require('nvim-autopairs.rule')
+      local cond = require('nvim-autopairs.conds')
+      
       npairs.setup(opts)
+
+      -- Add spaces between parentheses
+      local brackets = { { '(', ')' }, { '[', ']' }, { '{', '}' } }
+      npairs.add_rules {
+        Rule(' ', ' ')
+          :with_pair(function (opts)
+            local pair = opts.line:sub(opts.col - 1, opts.col)
+            return vim.tbl_contains({
+              brackets[1][1]..brackets[1][2],
+              brackets[2][1]..brackets[2][2],
+              brackets[3][1]..brackets[3][2],
+            }, pair)
+          end)
+      }
+      for _, bracket in pairs(brackets) do
+        npairs.add_rules {
+          Rule(bracket[1]..' ', ' '..bracket[2])
+            :with_pair(function() return false end)
+            :with_move(function(opts)
+              return opts.prev_char:match('.%'..bracket[2]) ~= nil
+            end)
+            :use_key(bracket[2])
+        }
+      end
+
+      -- LaTeX-specific rules
+      npairs.add_rules({
+        -- Add matching \] when typing \[
+        Rule("\\[", "\\]", "tex"),
+
+        -- Add matching $ when typing $
+        Rule("$", "$", "tex")
+          :with_pair(cond.not_before_regex("\\"))
+          :with_pair(cond.not_after_regex("\\"))
+          :with_pair(function(opts)
+            local last_char = opts.line:sub(opts.col - 1, opts.col - 1)
+            if last_char == "$" then
+              -- Don't pair if we're between $$
+              return false
+            end
+            return true
+          end),
+
+        -- Add matching $$ when typing $$
+        Rule("$$", "$$", "tex")
+          :with_pair(cond.not_before_regex("\\"))
+          :with_pair(cond.not_after_regex("\\"))
+      })
 
       -- If you want to automatically add `(` after selecting a function or method
       local cmp_autopairs = require('nvim-autopairs.completion.cmp')
