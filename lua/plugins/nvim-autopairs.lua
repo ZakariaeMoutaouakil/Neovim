15f8263ab633ec5e092fccdeefd62d479ab54c8b
Zakariae Moutaouakil <moutaouakilzakariae.cpgea@gmail.com>
2024-07-28 19:39:44 +0200

    Added nvim-autopairs.lua

---
+++ lua/plugins/nvim-autopairs.lua
@@ -0,0 +1,37 @@
+return {
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    opts = {
+      check_ts = true,
+      ts_config = {
+        lua = {'string'},  -- don't add pairs in lua string treesitter nodes
+        javascript = {'template_string'},  -- don't add pairs in javascript template_string treesitter nodes
+        java = false,  -- don't check treesitter on java
+      },
+      disable_filetype = { "TelescopePrompt" },
+      fast_wrap = {
+        map = '<M-e>',
+        chars = { '{', '[', '(', '"', "'" },
+        pattern = [=[[%'%"%)%>%]%)%}%,]]=],
+        end_key = '$',
+        keys = 'qwertyuiopzxcvbnmasdfghjkl',
+        check_comma = true,
+        highlight = 'Search',
+        highlight_grey='Comment'
+      },
+    },
+    config = function(_, opts)
+      local npairs = require("nvim-autopairs")
+      npairs.setup(opts)
+
+      -- If you want to automatically add `(` after selecting a function or method
+      local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+      local cmp = require('cmp')
+      cmp.event:on(
+        'confirm_done',
+        cmp_autopairs.on_confirm_done()
+      )
+    end,
+  }
+}
