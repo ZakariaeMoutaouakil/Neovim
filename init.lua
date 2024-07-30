876b17a61250863e6e88a812d681c3577d94191d
Zakariae Moutaouakil <moutaouakilzakariae.cpgea@gmail.com>
2024-07-30 10:39:20 +0200

    Added spell checker

--- init.lua
+++ init.lua
@@ -1,1 +1,12 @@
+-- Set the leader key
+vim.g.mapleader = "\\"
+vim.g.maplocalleader = "\\"
+print("The current leader key is: " .. vim.g.mapleader)
+
+-- Set Spell Checking
+vim.opt.spell = true
+vim.opt.spelllang = "en_us"
+
+-- Now load the lazy config module
 require("config.lazy")
+
